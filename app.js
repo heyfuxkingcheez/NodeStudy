c7bae73a97eab84c0dcb2caad23bd3fbad185beb
Your Name <you@example.com>
2023-10-31 19:23:02 +0900

    node 모듈 추가

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const goodsRouter = require('./route/goods.js');
+
+// app.get('/', (req, res) => {
+// res.send('Hello World!');
+// });
+
+// localhost:3000/ api -> goodsRouter
+app.use('/api', [goodsRouter]);
+
+app.listen(port, () => {
+    console.log(port, '포트로 서버가 열렸어요!');
+});
